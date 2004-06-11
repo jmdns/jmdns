0d20c688d7cd4af112a6f2029292b00833f231cc
rawcoder <rawcoder@653a831d-6b0a-4d54-b140-c52d7d628f2a>
2004-06-11 13:22:26 +0000

    RFE 868432, RFE 868433, RFE 892855, plus changes required to pass Rendezvous Conformance Test.

---
+++ src/javax/jmdns/DNSState.java
@@ -0,0 +1,96 @@
+/*
+ * @(#)DNSState.java  1.0  May 23, 2004
+ *
+ * Copyright (c) 2004 Werner Randelshofer
+ * Staldenmattweg 2, Immensee, CH-6405, Switzerland.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Werner Randelshofer. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Werner Randelshofer.
+ */
+
+package javax.jmdns;
+
+import java.util.*;
+/**
+ * DNSState defines the possible states for services registered with JmDNS.
+ *
+ * @author  Werner Randelshofer
+ * @version 1.0  May 23, 2004  Created.
+ */
+public class DNSState implements Comparable {
+    
+    private final String name;
+    
+    /** Ordinal of next state to be created. */
+    private static int nextOrdinal = 0;
+    /** Assign an ordinal to this state. */
+    private final int ordinal = nextOrdinal++;
+    /**
+     * Logical sequence of states.
+     * The sequence is consistent with the ordinal of a state.
+     * This is used for advancing through states.
+     */
+    private final static ArrayList sequence = new ArrayList();
+    
+    private DNSState(String name) {
+        this.name = name;
+        sequence.add(this);
+    }
+    public final String toString() { return name; }
+    
+    public static final DNSState PROBING_1 = new DNSState("probing 1");
+    public static final DNSState PROBING_2 = new DNSState("probing 2");
+    public static final DNSState PROBING_3 = new DNSState("probing 3");
+    public static final DNSState ANNOUNCING_1 = new DNSState("announcing 1");
+    public static final DNSState ANNOUNCING_2 = new DNSState("announcing 2");
+    public static final DNSState ANNOUNCED = new DNSState("announced");
+    public static final DNSState CANCELED = new DNSState("canceled");
+    
+    /**
+     * Returns the next advanced state.
+     * In general, this advances one step in the following sequence: PROBING_1,
+     * PROBING_2, PROBING_3, ANNOUNCING_1, ANNOUNCING_2, ANNOUNCED.
+     * Does not advance for ANNOUNCED and CANCELED state.
+     */
+    public final DNSState advance() {
+        return (this.isProbing() || this.isAnnouncing())
+        ? (DNSState) sequence.get(this.ordinal + 1)
+        : this;
+    }
+    
+    /**
+     * Returns to the next reverted state.
+     * All states except CANCELED revert to PROBING_1.
+     * Status CANCELED does not revert.
+     */
+    public final DNSState revert() {
+        return (this == CANCELED) ? this : PROBING_1;
+    }
+    
+    /**
+     * Returns true, if this is a probing state.
+     */
+    public boolean isProbing() {
+        return compareTo(PROBING_1) >= 0 && compareTo(PROBING_3) <= 0;
+    }
+    /**
+     * Returns true, if this is an announcing state.
+     */
+    public boolean isAnnouncing() {
+        return compareTo(ANNOUNCING_1) >= 0 && compareTo(ANNOUNCING_2) <= 0;
+    }
+    
+    /**
+     * Compares two states.
+     * The states compare as follows:
+     * PROBING_1 &lt; PROBING_2 &lt; PROBING_3 &lt; ANNOUNCING_1 &lt;
+     * ANNOUNCING_2 &lt; RESPONDING &lt; ANNOUNCED &lt; CANCELED.
+     */
+    public int compareTo(Object o) {
+        return ordinal - ((DNSState)o).ordinal;
+    }
+}
