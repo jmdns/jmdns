5050a353b3bf8db4b38f960b871744844e6c7edf
rickblair <rickblair@653a831d-6b0a-4d54-b140-c52d7d628f2a>
2005-07-17 03:40:34 +0000

    Added Apache Version 2 licenseS: ----------------------------------------------------------------------  Committing in .
     Modified Files:
     	build.xml jmdns.iws src/javax/jmdns/DNSCache.java
     	src/javax/jmdns/DNSConstants.java
     	src/javax/jmdns/DNSEntry.java src/javax/jmdns/DNSIncoming.java
     	src/javax/jmdns/DNSListener.java
     	src/javax/jmdns/DNSOutgoing.java
     	src/javax/jmdns/DNSQuestion.java
     	src/javax/jmdns/DNSRecord.java src/javax/jmdns/DNSState.java
     	src/javax/jmdns/HostInfo.java src/javax/jmdns/JmDNS.java
     	src/javax/jmdns/ServiceEvent.java
     	src/javax/jmdns/ServiceInfo.java
     	src/javax/jmdns/ServiceListener.java
     	src/javax/jmdns/ServiceTypeListener.java
     	src/javax/jmdns/package.html
     ----------------------------------------------------------------------

--- src/javax/jmdns/DNSListener.java
+++ src/javax/jmdns/DNSListener.java
@@ -12,6 +12,11 @@
  * with Werner Randelshofer.
  */
 
+//Copyright 2003-2005 Arthur van Hoff, Rick Blair
+//Licensed under Apache License version 2.0
+//Original license LGPL
+
+
 package javax.jmdns;
 
 // REMIND: Listener should follow Java idiom for listener or have a different
@@ -21,7 +26,7 @@ package javax.jmdns;
  * DNSListener.
  * Listener for record updates.
  *
- * @author Werner Randelshofer
+ * @author Werner Randelshofer, Rick Blair
  * @version 1.0  May 22, 2004  Created.
  */
 interface DNSListener
