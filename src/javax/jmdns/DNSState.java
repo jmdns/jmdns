9a8cda3aed8d376d424b1b55a9a267c5bfa078ff
rickblair <rickblair@653a831d-6b0a-4d54-b140-c52d7d628f2a>
2005-10-23 18:01:42 +0000

    Fixede Copyright notices RnB

--- src/javax/jmdns/DNSState.java
+++ src/javax/jmdns/DNSState.java
@@ -1,17 +1,3 @@
-/*
- * @(#)DNSState.java  1.0  May 23, 2004
- *
- * Copyright (c) 2004 Werner Randelshofer
- * Staldenmattweg 2, Immensee, CH-6405, Switzerland.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Werner Randelshofer. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Werner Randelshofer.
- */
-
 //Copyright 2003-2005 Arthur van Hoff, Rick Blair
 //Licensed under Apache License version 2.0
 //Original license LGPL
--- src/samples/TestShutdownHook.java
+++ src/samples/TestShutdownHook.java
@@ -1,17 +1,3 @@
-/*
- * @(#)TestShutdownHook.java  1.0  May 24, 2004
- *
- * Copyright (c) 2004 Werner Randelshofer
- * Staldenmattweg 2, Immensee, CH-6405, Switzerland.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Werner Randelshofer. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Werner Randelshofer.
- */
-
 package samples;
 
 import java.io.*;
