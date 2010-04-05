68dcdd8c1be67fc2b04219f3a2b317cc607759fb
spearway <spearway@653a831d-6b0a-4d54-b140-c52d7d628f2a>
2010-04-05 17:29:32 +0000

    Rev 3.1.7

--- src/javax/jmdns/JmDNS.java
+++ src/javax/jmdns/JmDNS.java
@@ -20,7 +20,7 @@ public abstract class JmDNS
     /**
      * The version of JmDNS.
      */
-    public static String VERSION = "3.1.6";
+    public static String VERSION = "3.1.7";
 
     /**
      * Create an instance of JmDNS.
