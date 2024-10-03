2907e192662bb692954d53ca2c8637894d9638e3
janossch <57434745+janossch@users.noreply.github.com>
2024-10-03 20:59:40 +0200

    Code cleanup in packages under javax.jmdns.impl.tasks. Remove unnecessary whitespaces, fix several typos in comments, rename local and field variables (remove leading "_"). (#317)
    Signed-off-by: Janos Toller <janossch@gmail.com>

--- src/main/java/javax/jmdns/impl/tasks/Responder.java
+++ src/main/java/javax/jmdns/impl/tasks/Responder.java
@@ -9,6 +9,7 @@ import java.net.InetSocketAddress;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Timer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,30 +24,18 @@ import javax.jmdns.impl.constants.DNSConstants;
  * The Responder sends a single answer for the specified service infos and for the host name.
  */
 public class Responder extends DNSTask {
-    private static final Logger             logger = LoggerFactory.getLogger(Responder.class);
-
-    /**
-     *
-     */
-    private final DNSIncoming _in;
-
-    /**
-     * The incoming address and port.
-     */
-    private final InetAddress _addr;
-    private final int         _port;
-
-    /**
-     *
-     */
-    private final boolean     _unicast;
+    private static final Logger logger = LoggerFactory.getLogger(Responder.class);
+    private final DNSIncoming dnsIncoming;
+    private final InetAddress inetAddress;
+    private final int port;
+    private final boolean unicast;
 
     public Responder(JmDNSImpl jmDNSImpl, DNSIncoming in, InetAddress addr, int port) {
         super(jmDNSImpl);
-        this._in = in;
-        this._addr = addr;
-        this._port = port;
-        this._unicast = (port != DNSConstants.MDNS_PORT);
+        this.dnsIncoming = in;
+        this.inetAddress = addr;
+        this.port = port;
+        this.unicast = (port != DNSConstants.MDNS_PORT);
     }
 
     /*
@@ -64,7 +53,7 @@ public class Responder extends DNSTask {
      */
     @Override
     public String toString() {
-        return super.toString() + " incoming: " + _in;
+        return super.toString() + " incoming: " + dnsIncoming;
     }
 
     /*
@@ -81,14 +70,14 @@ public class Responder extends DNSTask {
         // We respond after 20-120 ms if the query is truncated.
 
         boolean iAmTheOnlyOne = true;
-        for (DNSQuestion question : _in.getQuestions()) {
+        for (DNSQuestion question : dnsIncoming.getQuestions()) {
             logger.trace("{}.start() question={}", this.getName(), question);
             iAmTheOnlyOne = question.iAmTheOnlyOne(this.getDns());
             if (!iAmTheOnlyOne) {
                 break;
             }
         }
-        int delay = (iAmTheOnlyOne && !_in.isTruncated()) ? 0 : DNSConstants.RESPONSE_MIN_WAIT_INTERVAL + JmDNSImpl.getRandom().nextInt(DNSConstants.RESPONSE_MAX_WAIT_INTERVAL - DNSConstants.RESPONSE_MIN_WAIT_INTERVAL + 1) - _in.elapseSinceArrival();
+        int delay = (iAmTheOnlyOne && !dnsIncoming.isTruncated()) ? 0 : DNSConstants.RESPONSE_MIN_WAIT_INTERVAL + JmDNSImpl.getRandom().nextInt(DNSConstants.RESPONSE_MAX_WAIT_INTERVAL - DNSConstants.RESPONSE_MIN_WAIT_INTERVAL + 1) - dnsIncoming.elapseSinceArrival();
         if (delay < 0) {
             delay = 0;
         }
@@ -101,7 +90,7 @@ public class Responder extends DNSTask {
 
     @Override
     public void run() {
-        this.getDns().respondToQuery(_in);
+        this.getDns().respondToQuery(dnsIncoming);
 
         // We use these sets to prevent duplicate records
         Set<DNSQuestion> questions = new HashSet<>();
@@ -110,12 +99,11 @@ public class Responder extends DNSTask {
         if (this.getDns().isAnnounced()) {
             try {
                 // Answer questions
-                for (DNSQuestion question : _in.getQuestions()) {
+                for (DNSQuestion question : dnsIncoming.getQuestions()) {
                     logger.debug("{}.run() JmDNS responding to: {}", this.getName(), question);
 
                     // for unicast responses the question must be included
-                    if (_unicast) {
-                        // out.addQuestion(q);
+                    if (unicast) {
                         questions.add(question);
                     }
 
@@ -124,7 +112,7 @@ public class Responder extends DNSTask {
 
                 // remove known answers, if the TTL is at least half of the correct value. (See Draft Cheshire chapter 7.1.).
                 long now = System.currentTimeMillis();
-                for (DNSRecord knownAnswer : _in.getAnswers()) {
+                for (DNSRecord knownAnswer : dnsIncoming.getAnswers()) {
                     if (knownAnswer.isStale(now)) {
                         answers.remove(knownAnswer);
                         logger.debug("{} - JmDNS Responder Known Answer Removed", this.getName());
@@ -135,9 +123,9 @@ public class Responder extends DNSTask {
                 if (!answers.isEmpty()) {
                     logger.debug("{}.run() JmDNS responding", this.getName());
 
-                    DNSOutgoing out = new DNSOutgoing(DNSConstants.FLAGS_QR_RESPONSE | DNSConstants.FLAGS_AA, !_unicast, _in.getSenderUDPPayload());
-                    out.setDestination(new InetSocketAddress(_addr, _port));
-                    out.setId(_in.getId());
+                    DNSOutgoing out = new DNSOutgoing(DNSConstants.FLAGS_QR_RESPONSE | DNSConstants.FLAGS_AA, !unicast, dnsIncoming.getSenderUDPPayload());
+                    out.setDestination(new InetSocketAddress(inetAddress, port));
+                    out.setId(dnsIncoming.getId());
                     for (DNSQuestion question : questions) {
                         if (question != null) {
                             out = this.addQuestion(out, question);
@@ -145,15 +133,14 @@ public class Responder extends DNSTask {
                     }
                     for (DNSRecord answer : answers) {
                         if (answer != null) {
-                            out = this.addAnswer(out, _in, answer);
+                            out = this.addAnswer(out, dnsIncoming, answer);
 
                         }
                     }
                     if (!out.isEmpty()) this.getDns().send(out);
                 }
-                // this.cancel();
             } catch (Throwable e) {
-                logger.warn("{}run() exception ", this.getName(), e);
+                logger.warn("{}.run() exception ", this.getName(), e);
                 this.getDns().close();
             }
         }
