9175588f5d7bf9db7b2ade60737262512919a3ac
melloware <mellowaredev@gmail.com>
2024-10-10 15:34:59 -0400

    Cleanup and modernize Browser sample
    Signed-off-by: melloware <mellowaredev@gmail.com>

--- src/sample/java/samples/ListServices.java
+++ src/sample/java/samples/ListServices.java
@@ -25,7 +25,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-package samples;
+package javax.jmdns.tools;
 
 import java.io.IOException;
 import java.util.Enumeration;
