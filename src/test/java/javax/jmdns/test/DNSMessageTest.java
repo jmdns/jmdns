0e54b138ef6ef3afe169c9b712d2c2098668942d
Melloware <mellowaredev@gmail.com>
2024-10-11 17:03:31 -0400

    Fix License headers in all Java files (#320)
    Signed-off-by: melloware <mellowaredev@gmail.com>

--- src/test/java/javax/jmdns/test/DNSMessageTest.java
+++ src/test/java/javax/jmdns/test/DNSMessageTest.java
@@ -1,5 +1,15 @@
-/**
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package javax.jmdns.test;
 
@@ -20,9 +30,6 @@ import javax.jmdns.impl.constants.DNSRecordType;
 
 import org.junit.Test;
 
-/**
- *
- */
 public class DNSMessageTest {
 
     private static final byte[] nmap_scan_package = new byte[] { 0x30, (byte) 0x82, 0x00, 0x2f, 0x02, 0x01, 0x00, 0x04, 0x06, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, (byte) 0xa0, (byte) 0x82, 0x00, 0x20, 0x02, 0x04, 0x4c, 0x33, (byte) 0xa7, 0x56, 0x02,
